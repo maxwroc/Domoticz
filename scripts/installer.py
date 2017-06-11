b58e5e2b07e3b93af2f184cf8f2a6b592d6b310b
maxwroc <max.chodorowski@gmail.com>
2017-06-11 16:15:33 +0100

    List all deices for particular hardware

--- scripts/installer.py
+++ scripts/installer.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import sys, getopt, urllib, json
+import sys, getopt, urllib.request, json, pprint
 
 domoticz_host = 'localhost'
 domoticz_port = '8080'
@@ -8,7 +8,7 @@ hardware_name = "SmartPlug"
 
 def fetchJson(path):
     url = "http://" + domoticz_host + ":" + domoticz_port + path
-    response = urllib.urlopen(url)
+    response = urllib.request.urlopen(url)
 
     if response == None:
         raise ValueError("Getting response from host failed: " + url)
@@ -35,31 +35,42 @@ def createHardware():
 
     return obj
 
+def getListOfInstalledVirtualSensors(hardwareIdx):
+    result = fetchJson("/json.htm?type=devices&filter=all&used=true")
+    if result == None or "status" not in result or result["status"] != "OK":
+        raise ValueError("Failed to fetch list of devices from Domoticz")
+
+    print(result["status"])
+    return [device for device in result["result"] if "HardwareID" in device and device["HardwareID"] == hardwareIdx and print(device)]
 
 def main(argv):
     global domoticz_host, domoticz_port
     try:
-        opts, args = getopt.getopt(argv, "hd:p:")
+        opts, args = getopt.getopt(argv, "htd:p:")
     except getopt.GetoptError:
-        print 'install.py -d <domoticz_host> -p <domoticz_port>'
+        print('install.py -d <domoticz_host> -p <domoticz_port>')
     
     for opt, arg in opts:
         if opt == "-h":
-            print 'install.py -d <domoticz_host> -p <domoticz_port>'
+            print('install.py -d <domoticz_host> -p <domoticz_port>')
             sys.exit(2)
         elif opt == "-d":
             domoticz_host = arg
         elif opt == "-p":
             domoticz_port = arg
+        elif opt == "-t":
+            pp = pprint.PrettyPrinter(indent=4)
+            pp.pprint(getListOfInstalledVirtualSensors(5))
+            return
 
     data = fetchJson("/json.htm?type=hardware")
     smartPlug = getObjectWithProperty(data["result"], "Name", hardware_name)
 
     if smartPlug == None:
-        print "Creating new hardware for SmartPlug"
+        print("Creating new hardware for SmartPlug")
         smartPlug = createHardware()
     else:
-        print "Hardware for SmartPlug exists already - reusing"   
+        print("Hardware for SmartPlug exists already - reusing")
 
 
 
