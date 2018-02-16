8915cc9f07aff01ed4946110492acf4cb94cb69a
Maks <max.chodorowski@gmail.com>
2018-02-16 13:05:41 +0000

    Domoticz watchdog

---
+++ scripts/watchdog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function message() {
+    echo "Watchdog: $1"
+    pushover.sh "Watchdog: $1"
+}
+
+if curl --output /dev/null --silent --head --fail "http://localhost:8080"
+then
+    echo "Watchdog: Domoticz is running"
+else
+    message "Domoticz offline!!! Trying to start..."
+    # doing restart is safer than start
+    service domoticz.sh restart
+
+    # wait couple secs 
+    sleep 20
+
+    if curl --output /dev/null --silent --head --fail "http://localhost:8080"
+    then
+        message "Domoticz is running back again!"
+    fi
+fi
